85a536e836892ccee890d48a40b03fe46b877e7c
abasheryassin <abasheryassin@gmail.com>
2023-03-14 16:33:34 -0500

    update user and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require('mongoose').Types;
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 // create a new user
 function createUser(req, res) {
@@ -13,33 +13,55 @@ function getUsers(req, res) {
     User.find()
         .then((users) => res.json(users))
         .catch((err) => {
-            console.error({message: err});
+            console.error({ message: err });
             return res.status.json(err);
         });
 };
 
 // retrive a single user
 function getSingleUser(req, res) {
-    User.findOne({ _id: req.params.userId})
+    User.findOne({ _id: req.params.userId })
         .populate({ path: 'Thought', select: '-__v' })
         .select('-__v')
         .then((user) => {
-            if(!user){
+            if (!user) {
                 res.status(404).json({ message: 'No user with that ID' });
             } else {
                 res.json(user);
             }
+        })
+        .catch((err) => {
+            console.error({ message: err });
+            return res.status.json(err);
         });
 };
 
 // update a user by id
-function updateUser() {
-
+function updateUser(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId},
+        {username: req.body.username,
+        email: req.body.email})
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'No user with that ID' });
+            } else {
+                res.json(user);
+            }
+        })
+        .catch((err) => {
+            console.error({ message: err });
+            return res.status.json(err);
+        });
 };
 
 // delete user by id
-function deleteUser() {
-
+function deleteUser(req, res) {
+    User.deleteOne({ _id: req.params.userId })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 };
 
 // add friend to users friend list by id
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,6 +4,6 @@ const { createUser, getUsers, getSingleUser, updateUser, deleteUser, addFriend, 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser)
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
