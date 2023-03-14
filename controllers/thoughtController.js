1658081cab1d42a3222e724943d4167c72783f60
abasheryassin <abasheryassin@gmail.com>
2023-03-14 18:46:00 -0500

    create thought and push to user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,12 +3,26 @@ const { User, Thought } = require('../models');
 
 // create new thought
 function createThought(req, res) {
-
+    Thought.create(req.body)
+           .then((thought) => {
+             return User.findByIdAndUpdate(
+                { _id: req.body.userId},
+                { $push: { thoughts: thought._id } },
+            )})
+            .then((user) => {
+                if (!user) {
+                    res.status(404).json({ message: 'Thought found, but found no user with that ID'});
+                } else {
+                    res.json('Created a Thought')
+                }
+            })
+           .catch((err) => res.status(500).json(err));
 };
 
 // retrive all thoughts
 function getThoughts(req, res) {
-
+    Thought.find()
+           .then((thought) => res.json(user));
 };
 
 // retrive single thought
